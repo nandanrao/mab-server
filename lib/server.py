ce7a25f934af6b974874e092193f1fe05ad948d6
Nandan Rao <nandanmarkrao@gmail.com>
2018-05-26 21:38:11 +0200

    added versions and script for paying workers

--- lib/server.py
+++ lib/server.py
@@ -1,10 +1,11 @@
 from flask import Flask, request, Response
 from pymongo import MongoClient, ASCENDING, DESCENDING
 from bson.json_util import dumps
+from bson.objectid import ObjectId
 from flask_cors import CORS, cross_origin
-import os, sys, logging, json
-import logging
+import os, sys, logging, json, random
 import boto3
+import xml.etree.ElementTree as ET
 
 # Make app
 print('Making app: ' + __name__)
@@ -19,18 +20,115 @@ client = MongoClient(
 
 DB = 'mab-survey'
 
-# DO SOME AUTOMATED MTURK SHIT
-# @app.route('/foo', methods=['GET'])
-# def foo():
-#     session = boto3.Session(profile_name='bgse-us') # get rid of profile...
-#     client = session.client('mturk')
-#     res = client.list_hits()
-#     return Response(json.dumps(res), mimetype='application/json')
+# AWS MTURK CONSTANTS STUFF
+# endpoint_url = 'https://mturk-requester-sandbox.us-east-1.amazonaws.com'
+# qualification_complete = '3NM2AQCBQ46EBVNZD083TJFHTYP55Y'
+endpoint_url = 'https://mturk-requester.us-east-1.amazonaws.com'
+qualification_complete = '3UFT0YQ7M29DP46GHV3HV84IY48MSE'
+mturk = boto3.client('mturk', region_name='us-east-1', endpoint_url=endpoint_url)
+
+def get_all_hits(mturk):
+    ids = [hit['HITId'] for hit in mturk.list_hits()['HITs']]
+    return ids
+
+def get_workers_and_codes(mturk):
+    ids = get_all_hits(mturk)
+    assignments = [ass for i in ids
+               for ass in mturk.list_assignments_for_hit(HITId=i)['Assignments']]
+    answers = [a['Answer'] for a in assignments]
+    codes = [ET.fromstring(a)[0][1].text for a in answers]
+    worker_ids = [a['WorkerId'] for a in assignments]
+    assignment_ids = [a['AssignmentId'] for a in assignments]
+    return list(zip(worker_ids, codes, assignment_ids))
+
+def get_bonus(collection, code):
+    res = collection.find_one({'_id': ObjectId(code)})
+    if not res:
+        return 0
+    box = res['boxes'][-1]
+    wins = [1 if o['result'] == 'win' else 0 for o in box]
+    return sum(wins)
+
+def get_paid(mturk):
+    ids = get_all_hits(mturk)
+    bonuses = [mturk.list_bonus_payments(HITId = i)['BonusPayments']
+               for i in ids]
+    paid = [b['WorkerId'] for i in bonuses for b in i]
+    return paid
+
+def pay_all(mturk):
+    paid = get_paid(mturk)
+
+    li = [(w, c, a, get_bonus(client[DB].trial, c))
+          for w, c, a in get_workers_and_codes(mturk)]
+
+    li = [i for i in li if i[0] not in paid]
+    return [pay_worker(amt,c,w,a) for w,c,a,amt in li]
+
+def pay_worker(amt, code, worker_id, a_id):
+
+    # approve assignment
+    try:
+        mturk.approve_assignment(AssignmentId=a_id)
+    except Exception as e:
+        logging.error(e)
+        pass
+
+    # associate_qualification_with_worker
+    try:
+        mturk.associate_qualification_with_worker(
+            QualificationTypeId=qualification_complete,
+            WorkerId=worker_id
+        )
+    except Exception as e:
+        logging.error(e)
+        pass
+
+    # send_bonus
+    if amt > 0:
+        mturk.send_bonus(
+            WorkerId=worker_id,
+            BonusAmount='{}'.format(amt),
+            AssignmentId=a_id,
+            Reason='Survey Completed'
+        )
+
+    # Update in our db
+    return client[DB].trial.find_one_and_update(
+        { '_id': ObjectId(code)},
+        {'$set': { 'bonus_paid': amt }}
+    )
+
+def needed_treatment(res):
+    if not res:
+        return random.choice(['a', 'b'])
+    if len(res) > 1:
+        treat = sorted(res, key = lambda t: t[1])[0][0]
+    else:
+        treat = 'b' if res[0][0] == 'a' else 'a'
+    return treat
+
+def get_next_treatment(collection, version):
+    res = collection.aggregate([
+        { '$match': {'version': version}},
+        { '$group': { '_id': { 'treatment': '$treatment' }, 'count': { '$sum': 1}}}
+    ])
+    res = [ (d['_id']['treatment'], d['count']) for d in res]
+    return needed_treatment(res)
 
 @app.route('/submit', methods=['POST'])
 def submit():
     dat = request.json
     collection = client[DB].trial
-    res = collection.insert_one(dat)
-    res = { 'code': str(res.inserted_id) }
+    db_res = collection.insert_one(dat)
+    code = str(db_res.inserted_id)
+    res = { 'code': code }
+    return Response(json.dumps(res), mimetype='application/json')
+
+@app.route('/treatment', methods=['GET'])
+def get_treatment():
+    version = request.args.get('version', '0.1')
+    collection = client[DB].trial
+    treat = get_next_treatment(collection, version)
+    res = { 'treatment': treat }
     return Response(json.dumps(res), mimetype='application/json')
